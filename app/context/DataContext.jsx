0e22c8b15e713c2a25dd9e55320e8dff79ce186f
Roberto <rob.riveros.es@gmail.com>
2024-01-30 20:20:12 +0100

    Update DataContext.jsx

--- app/context/DataContext.jsx
+++ app/context/DataContext.jsx
@@ -1,5 +1,5 @@
 "use client";
-import cartReducer from "@/utils/CartReducer";
+import cartReducer from "@/utils/CartReducer.jsx";
 // import cartReducer from "@/utils/CartReducer";
 // import cartReducer from "../../utils/CartReducer";
 import React, { createContext, useEffect, useReducer, useState } from "react";
